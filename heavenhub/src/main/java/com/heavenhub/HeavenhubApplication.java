229804b3c02683c179b30d54ff3531acd12c2c2f
ansh-khare89 <anshkhare564@gmail.com>
2026-04-09 00:05:00 +0530

    removed KeepAliveScheduler because it was causing trouble in myy website

--- heavenhub/src/main/java/com/heavenhub/HeavenhubApplication.java
+++ heavenhub/src/main/java/com/heavenhub/HeavenhubApplication.java
@@ -2,10 +2,8 @@ package com.heavenhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class HeavenhubApplication {
 
 	public static void main(String[] args) {
--- heavenhub/src/main/java/com/heavenhub/config/AppConfig.java
+++ heavenhub/src/main/java/com/heavenhub/config/AppConfig.java
@@ -1,14 +1,8 @@
 package com.heavenhub.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 }
